0658ce75a5dbf478a6d2ff83da6734368105dba1
Roman Pchelkin <epos.91@mail.ru>
2022-05-20 14:18:50 +0300

    fly

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const mainCard = document.getElementById('mainCard');
 const detailCard = document.getElementById('detailsCard');
+const beeFly = document.querySelector('.bee-img');
 
 mainCard.addEventListener('click', e => {
 	e.preventDefault();
 	mainCard.classList.add('card-up');
 	detailCard.classList.add('details-down')
+	beeFly.classList.add('bee-active')
 })
